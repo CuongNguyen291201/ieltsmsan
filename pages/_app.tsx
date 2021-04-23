b9d87a193b5b043fb7aa796f5d1e38be8105f817
duypv <duypv98@gmail.com>
2021-04-23 16:02:04 +0700

    add toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import { AppProps } from 'next/app';
 
 import '../styles/_global.scss';
 import { wrapper } from '../redux/store';
+import ToastBackgroundContainer from '../sub_modules/common/components/toast_container';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Component {...pageProps} />
+      <ToastBackgroundContainer />
     </>
   )
 }
