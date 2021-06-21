2f8c5c9734aeca7e2c233ac66263c8531371a525
Long <dohuylong572000@gmail.com>
2021-06-21 15:56:50 +0700

    add popup

--- components/CourseItem/index.tsx
+++ components/CourseItem/index.tsx
@@ -1,11 +1,12 @@
 import { useRouter } from 'next/router';
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { OtsvCategory } from '../../custom-types';
 import itemAvatar from '../../public/default/item-avatar.png';
 import { COURSE_DETAIL_PAGE_TYPE } from '../../sub_modules/share/constraint';
 import { Course } from '../../sub_modules/share/model/courses';
 import { getBrowserSlug, numberFormat } from '../../utils';
 import Container1 from '../containers/Container1';
+import PopupShowQuickView from '../popup-quick-view/index'
 import Ratings from '../Ratings';
 import './style.scss';
 
@@ -17,11 +18,26 @@ const CourseItem = (props: { category?: OtsvCategory; course: Course }) => {
   const onClickItem = useCallback(() => {
     router.push({ pathname: courseBrowserSlug, query: { root: category?._id ?? '' } });
   }, [course]);
-
+  const [showPopup, setShowPopup] = useState(false)
+  const clickShowPopup = () => {
+    setShowPopup(true)
+  }
   return (
     <Container1>
-      <div className="crs-avatar" onClick={onClickItem}>
-        <img src={course.avatar || itemAvatar} alt={course.name} />
+      <div className="crs-avatar">
+        {/* <img src={course.avatar || itemAvatar} alt={course.name} /> */}
+        <div className="hover-cat-item">
+          <div className="image-corse-item_">
+            <img src={category.avatar || itemAvatar} alt={category.name} />
+          </div>
+          <div className="button-hover-course-item">
+            <button onClick={clickShowPopup}>Xem nhanh</button>
+            <PopupShowQuickView showPopup={showPopup} course={course} showPopupFunction={() => {
+              setShowPopup(false)
+            }} />
+            <button>Mua ngay </button>
+          </div>
+        </div>
       </div>
       <div className="crs-info" onClick={onClickItem}>
         <div className="crs-title">
