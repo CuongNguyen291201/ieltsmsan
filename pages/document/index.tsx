05b39219e4ac6732476bb8711bf256ab820a9527
chiendv <duongvanchien1003@gmail.com>
2021-06-26 11:20:29 +0700

    update

--- pages/document/index.tsx
+++ pages/document/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import dynamic from 'next/dynamic';
 // const DocumentUI = dynamic(() => import('../../sub_modules/document/src/App'), { ssr: false });
 const ROOT_DOCUMENT_CATEGORY_ID = "60d147b2de1984563685542b";
 import DocumentUI from '../../sub_modules/document/src/App';
