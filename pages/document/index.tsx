d3c0a559fc4aaf2b2587c715117437a725aa0e3e
chiendv <duongvanchien1003@gmail.com>
2021-06-26 10:08:12 +0700

    refactor

--- pages/document/index.tsx
+++ pages/document/index.tsx
@@ -1,8 +1,4 @@
 import React, { useEffect } from 'react';
-import { Header } from '../../components/Document/Header'
-import MainHeader from '../../components/MainHeader';
-import Layout from '../../components/Layout';
-import dynamic from 'next/dynamic';
 // const DocumentUI = dynamic(() => import('../../sub_modules/document/src/App'), { ssr: false });
 const ROOT_DOCUMENT_CATEGORY_ID = "60d147b2de1984563685542b";
 import DocumentUI from '../../sub_modules/document/src/App';
@@ -15,10 +11,6 @@ const DocumentPage = () => {
             {(typeof window === 'undefined') ? null : <DocumentUI rootDocumentId={ROOT_DOCUMENT_CATEGORY_ID} />}
         </div>
     )
-    return (
-        <DocumentUI
-            rootDocumentId={ROOT_DOCUMENT_CATEGORY_ID}
-        />);
 };
 
 export default DocumentPage;
