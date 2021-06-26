4f1e1959a1a94cbc13ba9cba7fe932b970d7ab4d
kiennpt <nguyenphuctrungkien@gmail.com>
2021-06-26 08:12:44 +0700

    add document module

--- components/Document/Header.tsx
+++ components/Document/Header.tsx
@@ -11,32 +11,7 @@ export const Header = () => {
     console.log(isShowMenu)
     return (
         <div className="document-background">
-            <div className="container">
-                <div className="document-header">
-                    <div className="document-header-element">
-                        <input type="checkbox" className="nav-input" hidden id="menu-dropdown" checked={isShowMenu}/>
-                        <Avatar src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Logo_PTIT.jpg/768px-Logo_PTIT.jpg" />
-                        <span className="school-name" onClick={() => setIsShowMenu(!isShowMenu)}>Học viện Bưu chính viễn thông</span>
-                        <div className="menu-school">
-                            <Dropdown />
-                        </div>
-                    </div>
-                    <div className="document-header-element">
-                        <Input
-                            placeholder="Tên môn học...."
-                            suffix={
-                                <Tooltip title="Tìm kiếm môn học">
-                                    <SearchOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
-                                </Tooltip>
-                            }
-                        />
-                    </div>
-
-                    <div className="document-header-buttons document-header-element">
-                        <Button type="primary" style={{ backgroundColor: 'red' }} className="document-header-button">Đóng góp tài liệu</Button>
-                    </div>
-                </div>
-            </div>
+           AAAA
         </div>
 
 
--- pages/document/index.tsx
+++ pages/document/index.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Header } from '../../components/Document/Header'
 import MainHeader from '../../components/MainHeader';
 import Layout from '../../components/Layout';
-
-const Document = () => {
+import dynamic from 'next/dynamic';
+const DocumentUI = dynamic(() => import('../../sub_modules/document/src/App'), { ssr: false });
+const ROOT_DOCUMENT_CATEGORY_ID = "60d147b2de1984563685542b";
+const DocumentPage = () => {
+    return (
+        <DocumentUI
+            rootDocumentId={ROOT_DOCUMENT_CATEGORY_ID}
+        />);
     return (
         <Layout>
             <MainHeader />
-            <Header />
         </Layout>
     );
 };
 
-export default Document;
+export default DocumentPage;
