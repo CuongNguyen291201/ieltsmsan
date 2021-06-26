f105e14739ad7bf2a960eb3596c945d8c4e51719
chiendv <duongvanchien1003@gmail.com>
2021-06-26 17:03:48 +0700

    update router

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withSass = require('@zeit/next-sass');
 const withImages = require('next-images');
 const withPlugins = require('next-compose-plugins');
 
+
 module.exports = withPlugins([withCSS, withSass, withImages, {
 	async rewrites() {
 		return [
--- pages/document/index.tsx
+++ pages/document/index.tsx
@@ -1,16 +1,17 @@
 import React, { useEffect } from 'react';
 import dynamic from 'next/dynamic';
-// const DocumentUI = dynamic(() => import('../../sub_modules/document/src/App'), { ssr: false });
+const DocumentUI = dynamic(() => import('../../sub_modules/document/src/App'), { ssr: false });
 const ROOT_DOCUMENT_CATEGORY_ID = "60d147b2de1984563685542b";
-import DocumentUI from '../../sub_modules/document/src/App';
+// import DocumentUI from '../../sub_modules/document/src/App';
 const DocumentPage = () => {
-    useEffect(() => {
-        console.log("isServer", !(typeof window === 'undefined'));
-    }, [])
+    // useEffect(() => {
+    //     console.log("isServer", !(typeof window === 'undefined'));
+    // }, [])
     return (
-        <div suppressHydrationWarning>
-            {(typeof window === 'undefined') ? null : <DocumentUI rootDocumentId={ROOT_DOCUMENT_CATEGORY_ID} />}
-        </div>
+        <DocumentUI rootDocumentId={ROOT_DOCUMENT_CATEGORY_ID} />
+        // <div suppressHydrationWarning>
+        //     {typeof window === 'undefined' ? null : <DocumentUI rootDocumentId={ROOT_DOCUMENT_CATEGORY_ID} />}
+        // </div>
     )
 };
 
