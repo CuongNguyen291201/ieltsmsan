981e1ececf0d44c9b30d6e4bc4b66caa6c008c91
tú nguyễn <trongtrongtu169@gmail.com>
2021-07-06 17:04:00 +0700

    modify view

--- pages/khoa-hoc-cua-toi/index.tsx
+++ pages/khoa-hoc-cua-toi/index.tsx
@@ -7,6 +7,7 @@ import { useSelector } from "react-redux";
 import Footer from "../../components/Footer/index";
 import Header from '../../components/MainHeader/index';
 import Menu from '../../components/MainMenu/index';
+import Layout from '../../components/Layout';
 import SearchBox from "../../components/SearchBox";
 import itemAvatar from '../../public/default/item-avatar.png';
 import { AppState } from "../../redux/reducers";
@@ -24,86 +25,88 @@ const MyCoursePage = () => {
     const [courses, setCourses] = useState([]);
     const currentUser = useSelector((state: AppState) => state.userReducer.currentUser);
 
-    
+
     useEffect(() => {
         if (!!currentUser) {
             apiGetMyCourses(currentUser?._id)
-            .then((courses) => {
-                setCourses(courses);
-            })
+                .then((courses) => {
+                    setCourses(courses);
+                })
         }
     }, [currentUser]);
-    
+
     const onClickItem = useCallback((course: Course) => {
         const courseSlug = getBrowserSlug(course.slug, COURSE_DETAIL_PAGE_TYPE, course._id);
         router.push({ pathname: courseSlug });
     }, [courses]);
-    
-    return (
-        <div className="my-course">
-            <Header />
-            <Menu />
-            <div className="wrapper-my-course">
-                <div className="container">
-                    <div className="title-search">
-                        <h2>Khoá học của tôi</h2>
-                        <div>
-                            <SearchBox />
-                        </div>
-                    </div>
-                    <div className="wrapper-item-my-course">
-                        {courses.map((e) => {
-                            const nameCourse = e.name
-                            const shortDesc = e.shortDesc
-                            return (
-                                <div onClick={() => onClickItem(e)} key={e._id} className="my-course-item">
-                                    <div className="image-my-course">
-                                        <img src={e.avatar || itemAvatar} alt={e.name} />
-                                        <div className="hover-my-course">
-                                            <div className="detail-my-course">Chi tiết khoá học </div>
-                                        </div>
-                                    </div>
 
-                                    <div className="my-course-infor">
-                                        {nameCourse.length > 40 ? <Tooltip title={nameCourse} placement="bottom">
-                                            <div className="crs-title dot-1">{nameCourse} </div>
-                                        </Tooltip> : <div className="crs-title dot-1">{nameCourse} </div>}
-                                        <div>
-                                            {shortDesc.length > 60 ? <Tooltip title={shortDesc} placement="bottom">
-                                                <div className="crs-desc dot-2">{shortDesc} </div>
-                                            </Tooltip> : <div className="crs-desc dot-2">{shortDesc} </div>}
+    return (
+        <Layout>
+            <div className="my-course">
+                <Header />
+                <Menu />
+                <div className="wrapper-my-course">
+                    <div className="container">
+                        <div className="title-search">
+                            <h2>Khoá học của tôi</h2>
+                            <div>
+                                <SearchBox />
+                            </div>
+                        </div>
+                        <div className="wrapper-item-my-course">
+                            {courses.map((e) => {
+                                const nameCourse = e.name
+                                const shortDesc = e.shortDesc
+                                return (
+                                    <div onClick={() => onClickItem(e)} key={e._id} className="my-course-item">
+                                        <div className="image-my-course">
+                                            <img src={e.avatar || itemAvatar} alt={e.name} />
+                                            <div className="hover-my-course">
+                                                <div className="detail-my-course">Chi tiết khoá học </div>
+                                            </div>
                                         </div>
-                                        {/* <div className="crs-rating">
+
+                                        <div className="my-course-infor">
+                                            {nameCourse.length > 40 ? <Tooltip title={nameCourse} placement="bottom">
+                                                <div className="crs-title dot-1">{nameCourse} </div>
+                                            </Tooltip> : <div className="crs-title dot-1">{nameCourse} </div>}
+                                            <div>
+                                                {shortDesc.length > 60 ? <Tooltip title={shortDesc} placement="bottom">
+                                                    <div className="crs-desc dot-2">{shortDesc} </div>
+                                                </Tooltip> : <div className="crs-desc dot-2">{shortDesc} </div>}
+                                            </div>
+                                            {/* <div className="crs-rating">
                                             <div className="crs-point">{String(4.6).replace('.', ',')}</div>
                                             <div className="vote-star">
                                                 <Rate style={{ fontSize: '15px', color: '#ec1f24' }} disabled allowHalf defaultValue={4.5} />
                                             </div>
                                             <div className="crs-mem">({500})</div>
                                         </div> */}
-                                        <div className="crs-rating">
-                                            <div className="crs-point">{String(4.6).replace('.', ',')}</div>
-                                            <div className="vote-star">
-                                                <Rate style={{ fontSize: '15px', color: '#ec1f24' }} disabled allowHalf defaultValue={4.5} />
+                                            <div className="crs-rating">
+                                                <div className="crs-point">{String(4.6).replace('.', ',')}</div>
+                                                <div className="vote-star">
+                                                    <Rate style={{ fontSize: '15px', color: '#ec1f24' }} disabled allowHalf defaultValue={4.5} />
+                                                </div>
+                                                <div className="crs-mem">({500})</div>
                                             </div>
-                                            <div className="crs-mem">({500})</div>
-                                        </div>
-                                        <div className="crs-price">
-                                            <div className="crs-discount-price">{numberFormat.format(e.cost - e.discountPrice)} VNĐ</div>
-                                            {e.discountPrice !== 0 && <div className="crs-origin-price">{numberFormat.format(e.cost)} VNĐ</div>}
-                                        </div>
-                                        <div className="btn-video">
-                                            Video
+                                            <div className="crs-price">
+                                                <div className="crs-discount-price">{numberFormat.format(e.cost - e.discountPrice)} VNĐ</div>
+                                                {e.discountPrice !== 0 && <div className="crs-origin-price">{numberFormat.format(e.cost)} VNĐ</div>}
+                                            </div>
+                                            <div className="btn-video">
+                                                Video
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        })}
+                                )
+                            })}
+                        </div>
                     </div>
-                </div>
 
+                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
+        </Layout>
     )
 }
 
