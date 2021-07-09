f68915fc9ca5e7c849c639ab08989a27fe9b7a38
chiendv <duongvanchien1003@gmail.com>
2021-07-09 16:41:32 +0700

    update document

--- pages/document/index.tsx
+++ pages/document/index.tsx
@@ -9,10 +9,10 @@ import { GetServerSideProps } from 'next';
 const DocumentUI = dynamic(() => import('../../sub_modules/document/src/App'), { ssr: false });
 const ROOT_DOCUMENT_CATEGORY_ID = "60d147b2de1984563685542b";
 // import DocumentUI from '../../sub_modules/document/src/App';
-// export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(async ({ store, query, req }) => {
-//     const userInfo = await getUserFromToken(req);
-//     if (userInfo) store.dispatch(loginSuccessAction(userInfo));
-// });
+export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(async ({ store, query, req }) => {
+    const userInfo = await getUserFromToken(req);
+    if (userInfo) store.dispatch(loginSuccessAction(userInfo));
+});
 const DocumentPage = () => {
     // useEffect(() => {
     //     console.log("isServer", !(typeof window === 'undefined'));
@@ -20,18 +20,18 @@ const DocumentPage = () => {
     const { currentUser } = useSelector((state: any) => state.userReducer);
 
     return (
-        <DocumentUI rootDocumentId={ROOT_DOCUMENT_CATEGORY_ID} user={currentUser} />
+        <DocumentUI rootDocumentId={ROOT_DOCUMENT_CATEGORY_ID} />
         // <div suppressHydrationWarning>
         //     {typeof window === 'undefined' ? null : <DocumentUI rootDocumentId={ROOT_DOCUMENT_CATEGORY_ID} />}
         // </div>
     )
 };
-export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(async ({ store, req }) => {
-    const userInfo = await getUserFromToken(req);
-    if (userInfo) {
-        store.dispatch(loginSuccessAction(userInfo));
-    } else {
-        removeCookie(TOKEN);
-    }
-})
+// export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(async ({ store, req }) => {
+//     const userInfo = await getUserFromToken(req);
+//     if (userInfo) {
+//         store.dispatch(loginSuccessAction(userInfo));
+//     } else {
+//         removeCookie(TOKEN);
+//     }
+// })
 export default DocumentPage;
