0a877ac85b5a8d848e1cc9fd8cabc99bb62c6167
duypv <duypv98@gmail.com>
2021-08-21 18:50:48 +0700

    chore: change style breadcrumbs. remove redundant rating. add responsive root

--- components/Breadcrumb/index.tsx
+++ components/Breadcrumb/index.tsx
@@ -8,11 +8,11 @@ const _BreadcrumbItem = (props: { name: string; slug?: string; isEnd?: boolean; 
   return (
     <>
       <span className={`breadcrumb-item${isEnd ? ' active' : ''}`} onClick={() => {
-        router.push(slug);
+        if (!isEnd) router.push(slug);
       }}>
         {name}
       </span>
-      {!isEnd && <span className="breadcrumb-slash">/</span>}
+      {!isEnd && <span className="breadcrumb-slash"><i className="fas fa-caret-right" /></span>}
       {/* <i className="fas fa-caret-right breadcrumb-caret" />} */}
     </>
   )
@@ -23,14 +23,16 @@ const BreadcrumbItem = memo(_BreadcrumbItem);
 const Breadcrumb = (props: { items: Array<{ name: string; slug?: string; }> }) => {
   const { items } = props;
   return (
-    <div className="container breadcrumb">
-      <div className="main-content">
-        <BreadcrumbItem name="Trang chủ" />
-        {items.map((item, index) => (
-          <Fragment key={index}>
-            <BreadcrumbItem name={item.name} slug={item.slug} isEnd={index === items.length - 1} />
-          </Fragment>
-        ))}
+    <div className="breadcrumb">
+      <div className="container">
+        <div className="main-content">
+          <BreadcrumbItem name="Trang chủ" />
+          {items.map((item, index) => (
+            <Fragment key={index}>
+              <BreadcrumbItem name={item.name} slug={item.slug} isEnd={index === items.length - 1} />
+            </Fragment>
+          ))}
+        </div>
       </div>
     </div>
   )
