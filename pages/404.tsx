1d50a81f16fd3c5c9ff152a7de767047d2f7c4a1
duypv <duypv98@gmail.com>
2022-01-04 14:17:54 +0700

    feat: new 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,12 +4,14 @@ import { wrapper } from "../redux/store"
 import WebInfo from "../sub_modules/share/model/webInfo"
 import WebSocial from "../sub_modules/share/model/webSocial"
 import { apiGetPageLayout } from "../utils/apis/pageLayoutApi"
+// @ts-ignore
+import NotFoundImage from "../public/images/icons/not-found.svg";
 
 const ErrorNotFound = (props: {
   webInfo?: WebInfo, webSocial?: WebSocial
 }) => {
   return <Layout {...props}>
-    <ErrorView message="Không tìm thấy trang" />
+    <ErrorView message="Không tìm thấy trang" description="Page Not Found" errorCode={404} img={NotFoundImage} />
   </Layout>
 }
 
